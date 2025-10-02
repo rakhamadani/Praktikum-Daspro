8a09f8a668593f3096ebecdb258bfc5d4fe76d7e
rakhamadani <rakhamadani789@gmail.com>
2025-10-02 15:14:54 +0700

    java

--- Git.java
+++ Git.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class Git {
+    Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    System.out.println("Hello, World!");    
+    }
 
 }
---
+++ jobsheet5/ifCetakKRS23.java
@@ -0,0 +1,11 @@
+package jobsheet5;
+
+import java.util.Scanner;
+
+public class ifCetakKRS23 {
+    Scanner sc = new Scanner(System.in);
+   public static void main(String[] args) {
+      System.out.println("Cetak KRS");
+
+   }
+}
