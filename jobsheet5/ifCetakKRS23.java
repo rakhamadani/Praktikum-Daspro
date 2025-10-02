9628648d72aa10076a52d6f115f9390fcd0385bc
rakhamadani <rakhamadani789@gmail.com>
2025-10-02 16:06:48 +0700

    rakha

--- jobsheet5/ifCetakKRS23.java
+++ jobsheet5/ifCetakKRS23.java
@@ -3,9 +3,25 @@ package jobsheet5;
 import java.util.Scanner;
 
 public class ifCetakKRS23 {
-    Scanner sc = new Scanner(System.in);
-   public static void main(String[] args) {
-      System.out.println("Cetak KRS");
+   public static void main(String[] args) {  
+   Scanner sc = new Scanner(System.in);
+   
+   int nilai;
+   System.out.println("Masukkan Nilai Anda : ");
+   nilai = sc.nextInt();
+
+   if (nilai >= 50) {
+       System.out.println("Tuntas");
+   } else {
+       System.out.println("Tidak Tuntas");
+   }
+   if ("tuntas".equals("Tuntas")) {
+       System.out.println("Selamat Anda Lulus");
+   } else {
+       System.out.println("Silahkan Mengulang");
+   }
+   
+   }
 
    }
-}
+
---
+++ jobsheet5/switchCetakKRS23.java
@@ -0,0 +1,9 @@
+package jobsheet5;
+
+public class switchCetakKRS23 {
+
+    public static void main(String[] args) {
+    System.out.println("Hello, World!");   
+        }
+    }
+
