9562c35287f9be22d24585044bb65c1f2078fa40
rakhamadani <rakhamadani789@gmail.com>
2025-10-23 10:52:15 +0700

    Percobaan 1

--- Git.java
+++ Git.java
@@ -1,7 +1,4 @@
-import java.util.Scanner;
-
 public class Git {
-    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         
     System.out.println("Hello, World!");    
