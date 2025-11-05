cbf71936c76c631834ad29cfd1d555a1462a958b
rakhamadani <rakhamadani789@gmail.com>
2025-11-06 00:27:15 +0700

    tugas 2

---
+++ Jobsheet8/tugas2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class tugas2 {
+public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+int n;
+
+do {
+    System.out.print("Masukkan nilai n (minimal 3): ");
+    n = scanner.nextInt();
+    if (n < 3) {
+        System.out.println("Nilai n harus minimal 3. Silakan coba lagi.");}
+    } while (n < 3);
+
+for (int i = 1; i <= n; i++) {
+    for (int j = 1; j <= n; j++) {    
+        if (i == 1 || i == n || j == 1 || j == n) {
+            System.out.print(n);
+        } else {
+            System.out.print(" ");
+        } 
+        System.out.print("  ");
+            
+}
+    System.out.println();
+}
+}
+}
