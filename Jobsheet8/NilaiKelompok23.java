00c4291838c2eebd6d0374f602b7db40b51a6348
rakhamadani <rakhamadani789@gmail.com>
2025-11-05 23:56:51 +0700

    Modifikasi Percobaaan 4

--- Jobsheet8/NilaiKelompok23.java
+++ Jobsheet8/NilaiKelompok23.java
@@ -5,27 +5,32 @@ public class NilaiKelompok23 {
 
 public static void main(String[] args) {
 Scanner sc = new Scanner (System.in);
+double rataTertinggi = 0;
+int kelompokTertinggi = 0;
 
 int i = 1;
 while (i <= 6) {
     System.out.println("Kelompok " + i);
-
+    
     float totalNilai = 0;
     for (int j=1; j<=5; j++) {
         System.out.print(" Nilai dari Kelompok Penilai " + j + ": ");
         int nilai = sc.nextInt();
         totalNilai += nilai; 
-    }
 
+        
+    }
+    
     float rataNilai = totalNilai / 5;
     System.out.println(" Rata-rata Nilai Kelompok " + i + " : " + rataNilai);
     i++;
-    
-
-
+    if (rataNilai > rataTertinggi) {
+        rataTertinggi = rataNilai;
+        kelompokTertinggi = i - 1;
+    }
 }
 
-
-
+System.out.println("Rata-rata Tertinggi: " + rataTertinggi);
+System.out.println("Kelompok dengan Rata-rata Tertinggi: Kelompok " + kelompokTertinggi);
 }
 }
