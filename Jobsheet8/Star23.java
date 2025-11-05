d57adcc5b54e6a2397cb5b6cca33e801b1c5740b
rakhamadani <rakhamadani789@gmail.com>
2025-11-05 22:44:58 +0700

    Percobaan 3

--- Jobsheet8/Star23.java
+++ Jobsheet8/Star23.java
@@ -7,7 +7,7 @@ Scanner sc = new Scanner(System.in);
 System.out.print("Masukkan nilai n = ");
 int n = sc.nextInt();
 
-for ( int i=1; i<=n; i++){
+for ( int i=1; i<=n; i++ ) {
     System.out.print("*");
 }
 
