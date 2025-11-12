4a6ac128bddae83bf8c28a5936aed5c6b67bbbc3
rakhamadani <rakhamadani789@gmail.com>
2025-11-13 01:31:26 +0700

    Tugas 1

---
+++ Jobsheet9/Tugas1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Tugas1 {
+public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+        
+System.out.print("Masukkan banyaknya nilai mahasiswa yang akan diinput: ");
+int jumlahNilai = sc.nextInt();
+int[] nilaiMahasiswa = new int[jumlahNilai];
+int total = 0;
+int nilaiTertinggi = 0;
+int nilaiTerendah = 101; 
+
+for (int i = 0; i < jumlahNilai; i++) {
+    System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+    int nilai = sc.nextInt();
+    nilaiMahasiswa[i] = nilai;
+    total += nilai;
+
+    if (nilai > nilaiTertinggi) {
+        nilaiTertinggi = nilai;
+    }
+            
+    if (nilai < nilaiTerendah) {
+        nilaiTerendah = nilai;
+    }
+}
+        
+System.out.println("\n--- Hasil Pengelolaan Nilai ---");
+
+System.out.print("Semua Nilai yang Dimasukkan: [");
+for (int i = 0; i < nilaiMahasiswa.length; i++) {
+    System.out.print(nilaiMahasiswa[i]);
+    if (i < nilaiMahasiswa.length - 1) {
+        System.out.print(", ");
+    }
+}
+        System.out.println("]");
+        
+        double rataRata = (double) total / jumlahNilai;
+        System.out.printf("Nilai Rata-rata Mahasiswa: %.2f\n", rataRata);
+
+        System.out.println("Nilai Tertinggi: " + nilaiTertinggi);
+        System.out.println("Nilai Terendah: " + nilaiTerendah);
+        
+        
+}
+}
