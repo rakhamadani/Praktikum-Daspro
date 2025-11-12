5912af22eb013c3983635de79b464d132d24875a
rakhamadani <rakhamadani789@gmail.com>
2025-11-13 00:43:08 +0700

    Modifikasi Percobaan 3

--- Jobsheet9/ArrayRataNilai23.java
+++ Jobsheet9/ArrayRataNilai23.java
@@ -5,19 +5,33 @@ public class ArrayRataNilai23 {
 public static void main(String[] args) {
 Scanner sc = new Scanner(System.in);
 
-int [] nilaiMhs = new int [10];
+System.out.print("Masukkan jumlah mahasiswa: ");
+int jumlahMhs = sc.nextInt();
+double totalLulus = 0; 
+double totalTidakLulus = 0;
+int jumlahTidakLulus = 0;
+int [] nilaiMhs = new int [jumlahMhs];
 double total = 0;
 double rata2;
+int jumlahLulus = 0;
 
 for ( int i = 0; i < nilaiMhs.length; i++) {
-    System.out.print("Masukkan nilai mahasiswa ke-"+i+" : ");
-    nilaiMhs[i] = sc.nextInt();}
+    System.out.print("Masukkan nilai mahasiswa ke-"+(i +1)+" : ");
+    nilaiMhs[i] = sc.nextInt();
 
-for ( int i = 0; i < nilaiMhs.length; i++) {
-    total += nilaiMhs[i];}
+    if (nilaiMhs[i] > 70) {
+    totalLulus += nilaiMhs[i];
+    jumlahLulus++;
+    }else {
+    totalTidakLulus += nilaiMhs[i];
+    jumlahTidakLulus++;}
 
-rata2 = total / nilaiMhs.length;
-System.out.println("Rata rata nilai = "+rata2);
+}
+
+double rataLulus = (jumlahLulus > 0) ? totalLulus / jumlahLulus : 0;
+double rataTidakLulus = (jumlahTidakLulus > 0) ? totalTidakLulus / jumlahTidakLulus : 0;
+System.out.println("Rata rata nilai lulus = "+rataLulus);
+System.out.println("Rata rata nilai tidak lulus = "+rataTidakLulus);
  
 }
 }
