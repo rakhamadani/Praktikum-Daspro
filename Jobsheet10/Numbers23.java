bbb6f63b0374f2aa869a6aefb7750fdb1e6eb7cf
rakhamadani <rakhamadani789@gmail.com>
2025-11-29 20:24:28 +0700

    Percobaan 3

---
+++ Jobsheet10/Numbers23.java
@@ -0,0 +1,11 @@
+public class Numbers23 {
+public static void main(String[] args) {
+
+int[][] myaNumbers = new int[3][];
+myaNumbers[0] = new int[5];
+myaNumbers[1] = new int[3];
+myaNumbers[2] = new int[1];
+
+}
+    
+}
